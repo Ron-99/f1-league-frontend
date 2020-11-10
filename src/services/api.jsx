cc17d58d2b436b2dcc49365f38e7cedf6e2c56cc
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-11-10 16:38:01 -0300

    Ajustado a imagem e descriçao do site

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import {getToken} from './auth';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://api.e-racebrasil.com.br/"
 });
 
 api.interceptors.request.use(async config => {
