a3b5c430eb789def1b5e3a36ab7a8511009e79d0
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-11-12 16:30:02 -0300

    Trocado a base url para PRD

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,14 +3,14 @@ import axios from 'axios'
 import {getToken} from './auth';
 
 // PRD
-// const api = axios.create({
-//     baseURL: "https://api.e-racebrasil.com.br"
-// });
+const api = axios.create({
+    baseURL: "https://api.e-racebrasil.com.br"
+});
 
 // STG
-const api = axios.create({
-    baseURL: "http://ec2-52-67-113-37.sa-east-1.compute.amazonaws.com:8080"
-});
+// const api = axios.create({
+//     baseURL: "http://ec2-52-67-113-37.sa-east-1.compute.amazonaws.com:8080"
+// });
 
 api.interceptors.request.use(async config => {
     const token = getToken();
