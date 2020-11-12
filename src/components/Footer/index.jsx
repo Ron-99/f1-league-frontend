fab4a2d5fbf79fed3f2ab162cec61210d4d6c5a3
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-11-11 22:59:32 -0300

    Corrigido bug onde o instagram nao abria em uma nova guia

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import { FooterStyle, Line, Copy } from './style';
+import {Icons, Icon} from '../../components/Icons/style';
+
+const Footer = ({home}) => (
+    <FooterStyle>
+
+        <Icons>
+            <Icon colorHover={"#6a737d"} color={home ? "#fff" :"#424242"} style={{fontSize: "25px"}} href="https://www.linkedin.com/in/ron99/" target="_blank"> 
+                <FontAwesomeIcon icon={faLinkedin} />
+            </Icon>
+
+            <Icon colorHover={"#6a737d"} color={home ? "#fff" :"#424242"} style={{fontSize: "25px"}} href="https://github.com/ron-99" target="_blank">
+                <FontAwesomeIcon icon={faGithub} />
+            </Icon>
+
+            <Icon colorHover={"#6a737d"} color={home ? "#fff" :"#424242"} style={{fontSize: "25px"}} href="https://www.instagram.com/ron99__/?hl=pt-br" target="_blank">
+                <FontAwesomeIcon icon={faInstagram} />
+            </Icon>
+        </Icons>
+        <Line color={home ? "#fff" :"#424242"}/>
+        <Copy>
+            2020 &copy; Desenvolvido por Ronaldo Gomes
+        </Copy>        
+    </FooterStyle>
+)
+
+export default Footer;
