f102f835606456557ec9a4d2cbbeb850adadf17f
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-12-01 17:54:25 -0300

    Criado um controle de usuario

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,9 +1,10 @@
 export const TOKEN_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYjNiZDM1ZmZiZjgxNjViYTI0NzQwMSIsImVtYWlsIjoicm9uYWxkb3NnMTk5OUBvdXRsb29rLmNvbSIsIm5hbWUiOiJSb25hbGRvIEdvbWVzIiwiaWF0IjoxNTg5MTgzOTgyLCJleHAiOjE1ODkyNzAzODJ9.daQJ13WSXI9o1irZy5xMrfaiulKlVtHiKQPL2lmaeJ0";
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
-export const login = token => {
-    localStorage.setItem(TOKEN_KEY, token);
-    
+export const getEmail = () => localStorage.getItem('email')
+export const login = data => {
+    localStorage.setItem(TOKEN_KEY, data.token);
+    localStorage.setItem('email', data.data.email);
 };
 export const logout = () => {
     localStorage.removeItem(TOKEN_KEY);
